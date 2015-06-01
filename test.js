81bd8fed199a07507f4cf89be560c3da26d24216
DancerCube <357633047@qq.com>
2015-06-01 14:28:06 +0800

    commit test.html test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 * @Author: anchen
 * @Date:   2015-06-01 13:27:42
 * @Last Modified by:   anchen
-* @Last Modified time: 2015-06-01 14:24:30
+* @Last Modified time: 2015-06-01 14:27:19
 */
 
 'use strict';
@@ -10,7 +10,6 @@ function test(){
     var oDiv = document.getElementById(id);
     var disX = 0;
     var disY = 0;
-    var disZ = 0;
     oDiv.onmouseover = function(){
         
     }
