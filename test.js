d17916e1a7010e3d80a1cba8c1c43d40ec713753
DancerCube <357633047@qq.com>
2015-06-01 14:02:22 +0800

    test.js modify2

--- test.js
+++ test.js
@@ -2,10 +2,13 @@
 * @Author: anchen
 * @Date:   2015-06-01 13:27:42
 * @Last Modified by:   anchen
-* @Last Modified time: 2015-06-01 13:50:09
+* @Last Modified time: 2015-06-01 13:55:02
 */
 
 'use strict';
 function test(){
-    
+    var oDiv = document.getElementById(id);
+    oDiv.onmouseover = function(){
+        
+    }
 }
